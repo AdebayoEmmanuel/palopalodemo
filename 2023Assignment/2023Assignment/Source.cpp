8bb61d871d1c7e420afcdb35e4f5ff9ed7d5c0e9
Emmanuel (Demilade) Adebayo <92952014+AdebayoEmmanuel@users.noreply.github.com>
2022-12-26 15:25:50 +0100

    Update Source.cpp

--- 2023Assignment/2023Assignment/Source.cpp
+++ 2023Assignment/2023Assignment/Source.cpp
@@ -6,27 +6,36 @@
 
 int main()
 {
-// Read the mountain names from the input files
-std::vectorstd::string filenames = {"Alps.txt", "Pyrenees.txt", "Carpathians.txt", "Icelandic Highlands.txt"};
-Mountains mountains(filenames);
+    // Read the mountain names from the input files
+    std::vector<std::string> filenames = {"alps.txt", "pyrenees.txt", "carpathians.txt", "icelandic_highlands.txt"};
+    Mountains mountains(filenames);
 
-// Start the quiz
-std::string input;
-int correct_answers = 0;
-int questions_asked = 0;
-while (input != "exit" && input != "quit") {
-    std::string mountain = mountains.getRandomMountain();
-    std::cout << "Which mountain range is " << mountain << " in? (Enter 'exit' or 'quit' to end the quiz)" << std::endl;
-    std::cin >> input;
+    // Start the quiz
+    std::string input;
+    int correct_answers = 0;
+    int questions_asked = 0;
+    while (true) {
+        // Get a random mountain
+        std::string mountain = mountains.getRandomMountain();
+        std::cout << "Which range is " << mountain << " in?" << std::endl;
+        std::getline(std::cin, input);
 
-    if (mountains.checkRange(mountain, input)) {
-        correct_answers++;
+        // Check if the user wants to exit
+        if (input == "exit" || input == "quit") {
+            break;
+        }
+
+        // Check the answer
+        if (mountains.checkRange(mountain, input)) {
+            std::cout << "Correct!" << std::endl;
+            correct_answers++;
+        } else {
+            std::cout << "Incorrect." << std::endl;
+        }
+        questions_asked++;
     }
-    questions_asked++;
-}
 
-// Display the final score
-std::cout << "Your score is " << correct_answers << "/" << questions_asked << std::endl;
-
-return 0;
+    // Print the score
+    std::cout << "You answered " << correct_answers << " out of " << questions_asked << " questions correctly." << std::endl;
+    return 0;
 }
